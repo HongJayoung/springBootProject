cdcf0860e25440630cfabf49afdb7b4687010ce8
HongJayoung <ghddlwkdud@naver.com>
2022-07-13 14:08:09 +0900

    sts_22.07.13

--- src/main/java/com/kosta/myapp/controller/WebBoardController.java
+++ src/main/java/com/kosta/myapp/controller/WebBoardController.java
@@ -1,14 +1,19 @@
 package com.kosta.myapp.controller;
 
+import java.security.Principal;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +23,13 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.kosta.myapp.repository.WebBoardRepository;
 import com.kosta.myapp.repository.WebReplyRepository;
+import com.kosta.myapp.security.MemberService;
 import com.kosta.myapp.vo.PageMaker;
 import com.kosta.myapp.vo.PageVO;
 import com.kosta.myapp.vo.relation.WebBoard;
 import com.querydsl.core.types.Predicate;
 
+//@CrossOrigin //포트번호가 다른 서버로 갈 수 있도록
 @Controller
 @RequestMapping("/board/*")
 public class WebBoardController {
@@ -32,6 +39,9 @@ public class WebBoardController {
 	
 	@Autowired
 	WebReplyRepository rRepo;
+	
+	@Autowired
+	MemberService mservice;
 	
 	@PostMapping("/delete.go")
 	public String deletePost(PageVO pageVO,WebBoard board, RedirectAttributes attr) {
@@ -81,8 +91,33 @@ public class WebBoardController {
 		return "board/boardView";
 	}
 	
+//	@GetMapping("/webboard/boardlist")    
+//	public void selectAll(Model model, Principal principal, Authentication authentication ) {
+//	System.out.println(“방법1:" + principal);
+//	UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+//	System.out.println(＂방법2:" + userDetails);
+//
+//	Object principal2 = SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
+//	UserDetails userDetails2 = (UserDetails)principal2; 
+//	System.out.println(＂방법3:" + userDetails2);
+//
+//	String mid = principal.getName();
+//	UserDetails userDetails3 =mservice.loadUserByUsername(mid);
+//	System.out.println(＂방법4:" +userDetails3);
+//	}
+
 	@GetMapping("/webBoardList.go")
-	public String boardList(@ModelAttribute PageVO pageVO, Model model, HttpServletRequest request) {
+	public String boardList(@ModelAttribute PageVO pageVO, Model model, 
+							HttpServletRequest request, Principal principal, 
+							Authentication authentication, HttpSession session) {
+		//-----------security test----------------
+		System.out.println("방법1 : " + principal);
+		System.out.println("방법2 : " + authentication.getPrincipal());
+		System.out.println("방법3 : " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+		System.out.println("방법4 : " + mservice.loadUserByUsername(principal.getName()));
+		System.out.println("방법5 : " + session.getAttribute("user"));
+		//----------------------------------------
+		
 		Map<String, Object> map = (Map<String, Object>) RequestContextUtils.getInputFlashMap(request);
 		if(map != null) {
 			String msg = (String)map.get("msg");
